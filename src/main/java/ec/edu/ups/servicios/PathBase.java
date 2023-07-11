d47b629c2865a195248b643b91126e950fa0c6c8
Bryam <bryam@gmail.com>
2023-07-10 20:29:44 -0500

    first commit

---
+++ src/main/java/ec/edu/ups/servicios/PathBase.java
@@ -0,0 +1,15 @@
+package ec.edu.ups.servicios;
+
+import java.util.Collection;
+import java.util.Iterator;
+import java.util.Locale;
+
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+@ApplicationPath("rs")
+public class PathBase extends Application{
+
+	
+}
